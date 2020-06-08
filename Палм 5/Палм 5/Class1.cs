98742bb348695cb614ebc42093c542e0a5c1c1db
andy337 <52741688+andy337@users.noreply.github.com>
2020-06-08 10:15:07 +0300

    Add files via upload

---
+++ Палм 5/Палм 5/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace struct_lab_student
+{
+    struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char mathematicsMark;
+        public char physicsMark;
+        public char informaticsMark;
+        public int scholarship;
+        public Student(string lineWithAllData)
+        {
+            string[] arrwithdata = lineWithAllData.Replace("-","2").Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            surName = arrwithdata[0];
+            firstName = arrwithdata[1];
+            patronymic = arrwithdata[2];
+            sex = char.Parse(arrwithdata[3]);
+            dateOfBirth = arrwithdata[4];
+            mathematicsMark = char.Parse(arrwithdata[5]);
+            physicsMark = char.Parse(arrwithdata[6]);
+            informaticsMark = char.Parse(arrwithdata[7]);
+            scholarship = int.Parse(arrwithdata[8]);
+        }
+    }
+}
---
+++ Палм 5/Палм 5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace struct_lab_student
+{
+    partial class Program
+    {
+        static Student[] ReadData(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            Student[] students = new Student[lines.Length];
+            for (var i = 0; i < lines.Length; i++)
+            {
+                students[i] = new Student(lines[i]);
+            }
+            return students;
+
+        }
+
+        static void runMenu(Student[] studs)
+        {
+            string path = @"C:\Users\Admin\source\repos\Палм 5\Палм 5\bin\Debug\netcoreapp3.1\data_new.txt";
+            foreach (var stud in studs)
+            {
+                if (stud.mathematicsMark == '2' || stud.physicsMark == '2' || stud.informaticsMark == '2')
+                {
+                    using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+                    {
+                        sw.Write("fname: {0}; marks: {1} {2} {3} 0\n", stud.firstName, stud.informaticsMark, stud.mathematicsMark, stud.physicsMark);
+                    }
+                    Console.WriteLine("fname: {0}; marks: {1} {2} {3} 0", stud.firstName, stud.informaticsMark, stud.mathematicsMark, stud.physicsMark);
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student[] studs = ReadData("input.txt");
+            runMenu(studs);
+        }
+    }
+}
